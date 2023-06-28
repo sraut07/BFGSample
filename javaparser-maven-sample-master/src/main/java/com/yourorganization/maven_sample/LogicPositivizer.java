92dd0eec734e00cd76cbbab7614bc7a7c248636d
Shubham <rautshubham00@gmail.com>
2023-06-28 11:39:44 +0530

    Added properties pwd, username

--- javaparser-maven-sample-master/src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ javaparser-maven-sample-master/src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -62,4 +62,6 @@ public class LogicPositivizer {
                         // appended with a path to "output"
                         .resolve(Paths.get("output")));
     }
+
+    //added comments
 }
